7e1c1fbc42d03d7dab7095f54fec11738d8f4709
fa8854 <fa8854@yandex.ru>
2022-09-19 13:12:05 +0300

    Домашняя работа по строкам

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        homeWork1();
+    }
+    private static void homeWork1(){
+        System.out.println("Задача 1");
+
+
+        System.out.println();
+    }
+    private static void homeWork2(){
+        System.out.println("Задача 2");
+        System.out.println();
+    }
+}
